13129b8f4ea80eef412b00904c7b97e3626affb0
Clement Goodness <clementgoodness16@gmail.com>
2025-12-23 06:12:34 +0100

    Added a notification entity to store notifications for both students and staffs

---
+++ src/session/Entities/Notification.entity.ts
@@ -0,0 +1,41 @@
+import { FacultyStaff } from "src/user/Entities/faculty-staff.entity";
+import { Student } from "src/user/Entities/student.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Notification {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 30
+    })
+    title: string;
+
+    @Column({
+        type: 'varchar',
+        nullable: false,
+        length: 400
+    })
+    message: string;
+    
+    @Column({
+        type: 'date',
+        nullable: false
+    })
+    createdAt: Date;
+    
+    @Column({
+        type: 'boolean',
+        nullable: false
+    })
+    isRead: boolean;
+    
+    @ManyToOne(() => Student, (student) => student.notifications, { nullable: true })
+    student: Student;
+
+    @ManyToOne(() => FacultyStaff, (staff) => staff.notifications, { nullable: true })
+    staff: FacultyStaff;
+}
